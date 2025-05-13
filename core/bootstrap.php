c2bca879f93f35393e5654fd9d960644103c131b
hunter4ass <egorilicu@gmail.com>
2025-05-13 22:19:58 +0700

    заменена ручная автозагрузка на Composer

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -1,18 +1,8 @@
 <?php
 
+require_once __DIR__ . '/../vendor/autoload.php';
+
 const DIR_CONFIG = '/../config';
-
-spl_autoload_register(function ($className) {
-    $paths = include __DIR__ . DIR_CONFIG . '/path.php';
-    $className = str_replace('\\', '/', $className);
-
-    foreach ($paths['classes'] as $path) {
-        $fileName = $_SERVER['DOCUMENT_ROOT'] . "/$paths[root]/$path/$className.php";
-        if (file_exists($fileName)) {
-            require_once $fileName;
-        }
-    }
-});
 
 function getConfigs(string $path = DIR_CONFIG): array {
     $settings = [];
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'egor-ilchuk/pop-it-mvc',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '2608217f983936db777e39e7472aa59c1ff8ca58',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'egor-ilchuk/pop-it-mvc' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '2608217f983936db777e39e7472aa59c1ff8ca58',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
